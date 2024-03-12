10d8ed9d10bec118cb8081ea96460d2707d6db98
egepancaroglu <egepancaroglu@gmail.com>
2024-03-12 23:24:17 +0300

    Added Hafta 3

---
+++ Hafta 3/src/test/java/com/egepancaroglu/hafta3/Hafta3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egepancaroglu.hafta3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hafta3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
